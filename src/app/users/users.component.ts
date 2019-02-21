c9557a5e4d7d0a8cdc48395668a91cb4dffafd15
pablo gr <pablousegreenj2ee@gmail.com>
2019-02-20 21:57:09 -0600

    for each elements from service

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -37,10 +37,54 @@ export class UsersComponent implements OnInit {
   users$: Object;
   constructor(private data: DataService) { }
 
-  ngOnInit() {
+  /*ngOnInit() {
     this.data.getUsers().subscribe(
       data=>this.users$= data
     );
+  }*/
+
+  ngOnInit() {
+    this.data.getUsers().subscribe(
+      dataRet =>{this.users$ = dataRet;
+      console.log("Users_data: ", dataRet);
+      
+
+      const dataObject = JSON.parse(JSON.stringify(dataRet));
+      dataObject.forEach((element, index) => {
+        console.log(index + " User "+element.name + " --mail: "+ element.email);
+      });
+      console.log("______________________________________________________________________");
+
+
+      let allUsers = dataRet;
+     
+      /*allUsers.forEach((element, index) => {
+        //console.log(index +" - User: "+JSON.stringify(element));
+       // console.log(dataRet[index]);
+        console.log("---------------------------------------------------------------------------------------");
+      });*/
+
+      const rev = new Review(dataRet[0]['name'], dataRet[0]['email']);
+      console.log("End: ", rev);
+      
+      const revs = new Reviews(dataRet[0]['name'], dataRet[0]['email']);
+      console.log("End1: ", revs);
+      });
   }
+
+}
+
+class Review {
+  constructor(private name: string, private email: string){
+    this.name = name;
+    this.email = email;
+  }    
+}
+
+class Reviews {
+  constructor(private name, private email){
+    this.name = name;
+    this.email = email;
+  } 
 
 }
